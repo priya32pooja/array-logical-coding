25e9c7b2ab6bbc1d52bf9a1069124c7c6c4999f2
Priya Kumari <113525298+priya32pooja@users.noreply.github.com>
2023-05-15 22:03:00 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,12 @@
+package array_logical_coding;
+
+public class Q1 {
+	public static void main(String[] args) {
+		int[] a1 = {2, 7, 9, 3, 12, 18, 21};
+		
+		System.out.println(a1.length);
+	}
+}
+
+// print the length of array
+// output :- 7
